3816b131c86d68ca8c0a01aa4a48d86cde328a1d
James <gekylin@gmail.com>
2019-04-02 21:55:32 +0800

    update

---
+++ spring-boot-nginx-proxy/src/main/java/com/james/sample/nginx/proxy/SpringBootNginxApplication.java
@@ -0,0 +1,13 @@
+package com.james.sample.nginx.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNginxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNginxApplication.class, args);
+    }
+
+}
---
+++ spring-boot-nginx-proxy/src/main/java/com/james/sample/nginx/proxy/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.james.sample.nginx.proxy.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+
+    public void index(){
+
+    }
+}
