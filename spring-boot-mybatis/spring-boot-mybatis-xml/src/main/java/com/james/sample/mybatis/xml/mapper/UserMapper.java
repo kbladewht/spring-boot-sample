85260023ac47b74ab052d57885abc2781f65c509
Kylin <gekylin@gmail.com>
2019-10-16 11:31:45 +0800

    update mybatis xml

--- spring-boot-mybatis/spring-boot-mybatis-xml/src/main/java/com/james/sample/mybatis/xml/mapper/UserMapper.java
+++ spring-boot-mybatis/spring-boot-mybatis-xml/src/main/java/com/james/sample/mybatis/xml/mapper/UserMapper.java
@@ -7,28 +7,19 @@ import java.util.List;
 
 public interface UserMapper {
 
-    @Insert("INSERT INTO user(username, password, email, avatar, sex, activated, create_time, update_time) " +
-            "VALUES(#{username}, #{password}, #{email}, #{avatar}, #{sex}, #{activated}, #{createTime}, #{updateTime})")
     void insert(User user);
 
-    @Update("UPDATE  user SET username = #{username},password = #{password},email= #{email},avatar= #{avatar}," +
-            "sex= #{sex},activated= #{activated},create_time= #{create_time},update_time= #{update_time} " +
-            "WHERE user_id = #{user_id}")
     void update(User user);
 
-    @Select("SELECT * FROM user WHERE username = #{username}")
     User getUserByUsername(String username);
 
-    @Select("SELECT * FROM user WHERE user_id = #{userId}")
     User getUserByUserId(int userId);
 
-    @Select("SELECT * FROM user")
     List<User> getAllUser();
 
     /**
      * 根据 id 删除用户信息
      */
-    @Delete("DELETE from user WHERE user_id = #{userId}")
     void deleteUser(int userId);
 
 }
--- spring-boot-mybatis/spring-boot-mybatis-xml/src/test/java/com/james/sample/mybatis/xml/SpringBootMybatisXmlApplicationTests.java
+++ spring-boot-mybatis/spring-boot-mybatis-xml/src/test/java/com/james/sample/mybatis/xml/SpringBootMybatisXmlApplicationTests.java
@@ -1,16 +1,47 @@
 package com.james.sample.mybatis.xml;
 
+import com.james.sample.mybatis.xml.entity.User;
+import com.james.sample.mybatis.xml.mapper.UserMapper;
+import com.james.sample.mybatis.xml.service.UserService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootMybatisXmlApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
     @Test
-    public void contextLoads() {
+    public void userMapperTest() {
+
+//        User user = new User();
+//        user.setUsername("Kylin");
+//        user.setPassword("jamesjames");
+//        user.setEmail("james@gmail.com");
+//        user.setAvatar("avatar");
+//        user.setSex((byte) 0);
+//        user.setActivated((byte) 0);
+//        user.setCreateTime(System.currentTimeMillis());
+//        user.setUpdateTime(System.currentTimeMillis());
+//
+//        userService.insertUser(user);
+
+        List<User> users = userService.getAllUser();
+
+        System.out.println("=================================");
+        System.out.println("用户数 ：" + users.size());
+        for (User user1 : users) {
+            System.out.println(user1.toString());
+        }
+
     }
 
 }
