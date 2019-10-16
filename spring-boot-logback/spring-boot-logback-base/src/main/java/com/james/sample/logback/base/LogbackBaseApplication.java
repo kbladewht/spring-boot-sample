2656318306a65fdd47c2dc4431800733f69cc17a
Kylin <gekylin@gmail.com>
2019-10-16 16:06:16 +0800

    add project spring-boot-logback

---
+++ spring-boot-logback/spring-boot-logback-base/src/main/java/com/james/sample/logback/base/LogbackBaseApplication.java
@@ -0,0 +1,13 @@
+package com.james.sample.logback.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackBaseApplication.class, args);
+    }
+
+}
