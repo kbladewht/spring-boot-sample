d92d2145e0957e56c4e6cf735a6fb4706010cc9f
Kylin <gekylin@gmail.com>
2019-10-18 09:18:23 +0800

    update

--- spring-boot-logback/spring-boot-logback-base/src/main/java/com/james/sample/logback/base/LogbackBaseApplication.java
+++ spring-boot-logback/spring-boot-logback-base/src/main/java/com/james/sample/logback/base/LogbackBaseApplication.java
@@ -1,10 +1,14 @@
 package com.james.sample.logback.base;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogbackBaseApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(LogbackBaseApplication.class, args);
--- spring-boot-logback/spring-boot-logback-base/src/test/java/com/james/sample/logback/base/LogbackBaseApplicationTests.java
+++ spring-boot-logback/spring-boot-logback-base/src/test/java/com/james/sample/logback/base/LogbackBaseApplicationTests.java
@@ -2,16 +2,22 @@ package com.james.sample.logback.base;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootLogbackBaseApplicationTests {
+public class LogbackBaseApplicationTests {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
     public void contextLoads() {
-
+        logger.debug("this is debug");
+        logger.info("this is info");
+        logger.error("this is error");
     }
 
 }
